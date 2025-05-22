1bc4db31d32f269d3dabdaef916a9b5a4acf78b3
Jasbootstudios <jasbootstudios@gmail.com>
2025-05-22 04:29:45 -0400

    Ejersicio_1 completado

---
+++ Ejersicio_1/index.js
@@ -0,0 +1,13 @@
+var counterNumber = function (n) {
+    var result = [];
+    for (var i = 0; i < n; i++) {
+        if (i < 2) {
+            result.push(i);
+        }
+        else {
+            result.push(result[i - 1] + result[i - 2]);
+        }
+    }
+    return result;
+};
+console.info(counterNumber(10));
---
+++ Ejersicio_1/index.ts
@@ -0,0 +1,14 @@
+
+const counterNumber = (n:number):number[] =>{
+    let result:number[] = [] ;
+    for(let i = 0; i < n; i++){
+        if(i<2){
+            result.push(i);
+        }else{
+            result.push(result[i-1] + result[i - 2]);
+        }
+    }
+    return result;
+} 
+
+console.info(counterNumber(10));
